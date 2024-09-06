8c0afe190457beccfe251c481442dcbbe54ae738
emil <emil12.05.2005@gmail.com>
2024-09-06 03:53:08 +0300

    normal parse

--- TurismApp/Backend/MainModule/models/Places.py
+++ TurismApp/Backend/MainModule/models/Places.py
@@ -1,63 +1,69 @@
-from __future__ import annotations
-
-from typing import List
-
+import requests
+from environs import Env
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
+from typing import List, Optional
+
+env = Env()
+env.read_env()
+
+key = env.str("YANDEX_KEY_FIND_PLACE")
+
+router = APIRouter(prefix="/PlacesForCategory", tags=["PlacesForCategory"])
 
 
-class SearchRequest(BaseModel):
-    request: str
-    results: int
-    skip: int
-    boundedBy: List[List[float]]
+class Coordinates(BaseModel):
+    longitude: float
+    latitude: float
 
 
-class Point(BaseModel):
-    type: str
-    coordinates: List[float]
+class HoursInterval(BaseModel):
+    from_time: str
+    to_time: str
 
 
-class SearchResponse(BaseModel):
-    found: int
-    Point: Point
-    boundedBy: List[List[float]]
-    display: str
+class Availability(BaseModel):
+    Everyday: Optional[bool]
+    Intervals: List[HoursInterval]
 
 
-class ResponseMetaData(BaseModel):
-    SearchRequest: SearchRequest
-    SearchResponse: SearchResponse
-
-
-class Properties(BaseModel):
-    ResponseMetaData: ResponseMetaData
-
-
-class GeocoderMetaData(BaseModel):
-    kind: str
+class Hours(BaseModel):
     text: str
-    precision: str
+    Availabilities: List[Availability]
 
 
-class Properties1(BaseModel):
-    GeocoderMetaData: GeocoderMetaData
-    description: str
+class FeatureDetails(BaseModel):
+    id: str
     name: str
-    boundedBy: List[List[float]]
-
-
-class Geometry(BaseModel):
-    type: str
-    coordinates: List[float]
-
-
-class Feature(BaseModel):
-    type: str
-    properties: Properties1
-    geometry: Geometry
 
 
 class Places(BaseModel):
-    type: str
-    properties: Properties
-    features: List[Feature]
+    name: str
+    address: Optional[str]
+    coordinates: Coordinates
+    hours: Optional[Hours]
+    features: Optional[List[FeatureDetails]]
+
+def parse_response(response: dict) -> List[Places]:
+    organizations = []
+    for feature in response.get('features', []):
+        properties = feature['properties']
+        geometry = feature['geometry']['coordinates']
+
+        organization = Places(
+            name=properties.get('name'),
+            address=properties.get('description'),
+            coordinates=Coordinates(
+                longitude=geometry[0],
+                latitude=geometry[1]
+            ),
+            hours=properties.get('CompanyMetaData', {}).get('Hours'),
+            features=[
+                FeatureDetails(id=feature.get('id'), name=feature.get('name'))
+                for feature in properties.get('CompanyMetaData', {}).get('Categories', [])
+            ]
+        )
+        organizations.append(organization)
+    return organizations
+
+
